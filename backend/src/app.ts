13a695a4035d6d39e9cd30635b1950cc3e5a8cd9
Vansh Gupta <vanshsja1512@gmail.com>
2026-07-06 17:10:40 +0530

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -9,7 +9,11 @@ import { registerAuthRoutes } from './routes/auth.routes';
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+    origin: true,
+    credentials: true
+}));
 app.use(express.json());
 app.use(logger);
 
